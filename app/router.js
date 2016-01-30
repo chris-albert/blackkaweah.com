4eb3666cd1d426af6a5cb3275c78b3b73ff5bfc1
creasetoph <creasetoph@creasetoph.com>
2016-01-29 16:28:26 -0800

    First commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('music');
+  this.route('video');
+  this.route('contact');
+  this.route('donate');
 });
 
 export default Router;
